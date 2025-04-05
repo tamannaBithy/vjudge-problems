e89b9ff9701f783be02a4b7909165c0e26f718af
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-04-05 20:23:38 +0600

    c-change

--- week-2/c-change.cpp
+++ week-2/c-change.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x;
+        cin >> x;
 
+        if (x == 100)
+            cout << 0 << endl;
+        else
+        {
+            int ans = 100 - x;
+            if (ans % 10 != 0)
+                cout << 10 * (ans / 10) << endl;
+            else
+                cout << ans << endl;
+        }
+    }
     return 0;
 }
