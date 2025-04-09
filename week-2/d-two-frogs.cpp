dab0cd073396420641412a062f190c2aaad43dc8
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2025-04-09 16:57:35 +0600

    two-frogs

--- week-2/d-two-frogs.cpp
+++ week-2/d-two-frogs.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if (abs(a - b) % 2 == 0)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
     return 0;
 }
